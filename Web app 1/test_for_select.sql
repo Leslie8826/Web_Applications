4133b7be053017a1d6ff4614f9a3d6e9b0f356c6
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-04-20 13:48:48 -0400

    Update test_for_select.sql

--- Web app 1/test_for_select.sql
+++ Web app 1/test_for_select.sql
@@ -1,8 +1,12 @@
 -- Below are the SQL statements for the creation of the MySQL database as well as the single table of employees 
--- and the insertion of data in that table create database test_for_select;
+-- and the insertion of data in that table 
+
+-- Creation of the database
+create database test_for_select;
 
 use test_for_select;
 
+-- Creation of the single table of the database
 create table Employees(
 id int not null,
 firstName varchar (255),
@@ -10,14 +14,17 @@ lastName varchar (255),
 yearlySalary int not null
 );
 
+-- Test to see if table is created
 select * from Employees;
 
+-- Insertion of data in the database
 insert into Employees values (1, 'Ella', 'Johns', 190000);
 insert into Employees values (2, 'Paul', ' Goge', 140000);
 
 insert into Employees values (3, 'Mary', 'Franck', 370000),
 (4, 'James', 'Williams', 189000);
 
+-- Test to check if data were inserted
 select * from Employees;
 
 
