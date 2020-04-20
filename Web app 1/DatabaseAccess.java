2964038ae7ae76ad13526015de1f0a25884253af
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-04-20 13:03:49 -0400

    Update DatabaseAccess.java

--- Web app 1/DatabaseAccess.java
+++ Web app 1/DatabaseAccess.java
@@ -13,11 +13,11 @@ public class DatabaseAccess extends HttpServlet{
 	   
       // JDBC driver name and database URL 
 	  final String JDBC_DRIVER = "com.mysql.jdbc.Driver"; 
-      final String DB_URL="jdbc:mysql://localhost:3306/test_for_select";
+      final String DB_URL="jdbc:mysql://localhost:3306/test_for_select"; // test_for_select is the name of the database
       
-      //  Database credentials
+      //  information of Database
       final String USER = "root";
-      final String PASS = "MySQL_021282";
+      final String PASS = "put password here";
       
       // Set response content type
       response.setContentType("text/html");
@@ -33,25 +33,14 @@ public class DatabaseAccess extends HttpServlet{
          "<body bgcolor = \"#f0f0f0\">\n" +
          "<h1 align = \"center\">" + title + "</h1>\n");
       
-           
-    /*  String docType =
-              "<!doctype html" +
-              "transitional//en\">\n";
-              out.println(docType +
-              "<html>\n" +
-              "<head><title>" + title + "</title></head>\n" +
-              "<body>");*/
-       
-      
       try {
          // Register JDBC driver
          Class.forName("com.mysql.jdbc.Driver");
 
-         // Open a connection
-         //Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+         // Open a connection to mysql database
          conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
-         // Execute SQL query
+         // Execute SQL select query
          stmt = conn.createStatement();
          String sql;
          sql = "SELECT id, firstName, lastName, yearlySalary FROM Employees";
@@ -59,18 +48,12 @@ public class DatabaseAccess extends HttpServlet{
 
          // Extract data from result set
          while(rs.next()){
-            //Retrieve by column name
+            //column name
             int id  = rs.getInt("id");
             String firstName = rs.getString("firstName");
             String lastName = rs.getString("lastName");
             int yearlySalary = rs.getInt("yearlySalary");
-
-            //Display values
-            /*out.println("ID: " + id + "<br>");
-            out.println(", First Name: " + firstName + "<br>");
-            out.println(", Last Name: " + lastName + "<br>");
-            out.println(", Yearly Salary: " + yearlySalary + "<br>");*/
-            
+ 
             out.println("<br> ID: " + id + " ");
             out.println(", First Name: " + firstName + " ");
             out.println(", Last Name: " + lastName + " ");
@@ -83,24 +66,21 @@ public class DatabaseAccess extends HttpServlet{
          stmt.close();
          conn.close();
       } catch(SQLException se) {
-         //Handle errors for JDBC
          se.printStackTrace();
       } catch(Exception e) {
-         //Handle errors for Class.forName
          e.printStackTrace();
       } finally {
-         //finally block used to close resources
          try {
             if(stmt != null)
                stmt.close();
          } catch(SQLException se2) {
-         } // nothing we can do
+         }
          try {
             if(conn!=null)
             conn.close();
          } catch(SQLException se) {
             se.printStackTrace();
-         } //end finally try
+         }
       } //end try
    }
 } 
